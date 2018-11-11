597f7c5355714e52fdb8c00c30db010a7afc4428
Srinivas <gino1610@gmail.com>
2018-11-11 14:19:29 -0800

    updates to api example

--- ntask-api/index.js
+++ ntask-api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import consign from "consign";
 
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req,res) => res.json({status: "NTask API"}));
+app.set("json spaces", 4);
+
+consign()
+    .include("models")
+    .include("routes")
+    .into(app);
 
 app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
 
