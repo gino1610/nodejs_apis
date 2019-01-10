dbd8e89ba555e8c9b9da361fd36c6187217409d1
Srinivas <gino1610@gmail.com>
2019-01-09 22:59:44 -0800

    Added Sqlite DB functionality (pages loads but errors out as User table is empty)

--- task-api/models/tasks.js
+++ task-api/models/tasks.js
@@ -1,10 +1,28 @@
-module.exports = app => {
-    return {
-        findAll: (params, callback) => {
-            return callback([
-                {title: "Buy shoes"},
-                {title: "Fix notebook"}
-            ]);
+module.exports = (sequelize, DataType) => {
+    const Tasks = sequelize.define("Tasks", {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        done: {
+            type: DataType.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
         }
+    });
+
+    Tasks.associate = function(models) {
+        Tasks.belongsTo(models.Users);
     };
+
+    return Tasks;
 };
+
