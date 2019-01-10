d8b7fccbb62ee49b2219f9ba9c8559062d536ebd
Srinivas <gino1610@gmail.com>
2019-01-09 21:53:09 -0800

    Sqlite DB setup

---
+++ task-api/libs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    database: "tasks",
+    username: "",
+    password: "",
+    params: {
+        dialect: "sqlite",
+        storage: "task.sqlite",
+        define: {
+            underscores: true
+        }
+    }
+};
